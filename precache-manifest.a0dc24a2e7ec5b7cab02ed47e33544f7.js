e26413b3f15cb5c326dbd69d96752090b2a01033
Senatova93 <nanniin160@gmail.com>
2020-06-24 08:25:38 +0300

    Updates

---
+++ precache-manifest.a0dc24a2e7ec5b7cab02ed47e33544f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2c3e1527ced6f6bec00d009c9465b1",
+    "url": "/ignatHW/index.html"
+  },
+  {
+    "revision": "5d2c226b3878bfe05531",
+    "url": "/ignatHW/static/css/main.30665bf2.chunk.css"
+  },
+  {
+    "revision": "15a046cbb7d3666ad52f",
+    "url": "/ignatHW/static/js/2.86b6d9b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ignatHW/static/js/2.86b6d9b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2c226b3878bfe05531",
+    "url": "/ignatHW/static/js/main.0f3e7c4b.chunk.js"
+  },
+  {
+    "revision": "c28f07d4d44c0ffe750a",
+    "url": "/ignatHW/static/js/runtime-main.599e0fb3.js"
+  }
+]);
